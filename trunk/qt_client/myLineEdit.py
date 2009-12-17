b9fb8bc628b968ddda572484689f91d29ecf5066
sperre <sperre@77988d9d-5455-4c8b-b1c9-919ab32b2d28>
2009-12-17 02:37:09 +0000

    I can haz eddited, Up/Down/Left/Right -keyactions. I can haz not mentioned Webkit, support on last commit.
    git-svn-id: https://omegav.no/svn/kikrit@21 77988d9d-5455-4c8b-b1c9-919ab32b2d28

---
+++ trunk/qt_client/myLineEdit.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from PyQt4.QtCore import QEvent, Qt, SIGNAL
+from PyQt4.QtGui import QLineEdit
+
+class MyLineEdit(QLineEdit):
+	def __init__(self, *args):
+		QLineEdit.__init__(self, *args)
+
+	def event(self, event):
+		if event.type() == QEvent.KeyPress:
+			k = event.key()
+			if k == Qt.Key_Left:
+				self.emit(SIGNAL("LeftPressed()"))
+				return True
+			elif k == Qt.Key_Right:
+				self.emit(SIGNAL("RightPressed()"))
+				return True
+			elif k == Qt.Key_Up:
+				self.emit(SIGNAL("UpPressed()"))
+				return True
+			elif k == Qt.Key_Down:
+				self.emit(SIGNAL("DownPressed()"))
+				return True
+			elif k == Qt.Key_Enter:
+				self.emit(SIGNAL("EnterPressed()"))
+				return True
+		return QLineEdit.event(self, event)
