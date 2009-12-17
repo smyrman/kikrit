2e8933e17b031a334d28efb9bd7ba548c6bd8ca5
Andreas Hopland Sperre <andreas.sperre@gmail.com>
2009-12-17 00:48:46 +0000

    Added tabs and stuff
    git-svn-id: https://omegav.no/svn/kikrit@20 77988d9d-5455-4c8b-b1c9-919ab32b2d28

--- trunk/qt_client/client.py
+++ trunk/qt_client/client.py
@@ -6,15 +6,20 @@ from os.path import dirname, abspath, join as path_join
 sys.path.append(path_join(abspath(dirname(__file__)), ".."))
 os_environ['DJANGO_SETTINGS_MODULE'] = 'django_kikrit.settings'
 
-from PyQt4.QtGui import QApplication
+from PyQt4.QtGui import QApplication, QTabWidget
 
-from qt_client.widgets import MainWidget
+from qt_client.mainWidget import MainWidget
+from qt_client.adminWidget import AdminWidget
 
 
 def main():
 	app = QApplication(sys.argv)
-	widget = MainWidget()
-	widget.show()
+	tabs = QTabWidget()
+
+	tabs.addTab(MainWidget(), "Main")
+	tabs.addTab(AdminWidget(), "Admin")
+
+	tabs.show()
 	return app.exec_()
 
 
--- trunk/qt_client/mainWidget.py
+++ trunk/qt_client/mainWidget.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from PyQt4.QtCore import SIGNAL
+from PyQt4.QtCore import SIGNAL, Qt 
 from PyQt4.QtGui import QWidget, QLineEdit, QLabel, QPushButton, QListView,\
-		QGridLayout
+		QGridLayout, QStackedWidget, QPixmap, QGraphicsView, QGraphicsScene
 
 from django_kikrit.merchandise.models import Merchandise
 
@@ -12,11 +12,13 @@ from qt_client.models import MerchandiseListModel
 
 class MainWidget(QWidget):
 	search_line = None
-	msg_field = None
+	status = None
 	add_button = None
 	rem_button = None
 	left_list = None
 	right_list = None
+	stack = None
+	graphics = None
 
 	def __init__(self, parent=None):
 		QWidget.__init__(self, parent)
@@ -25,7 +27,8 @@ class MainWidget(QWidget):
 		self.search_line = QLineEdit(u"")
 		self.search_line.grabKeyboard()
 
-		self.msg_field = QLabel()
+		self.status = QLabel()
+		self.status.setAlignment(Qt.AlignRight)
 
 		self.left_list = QListView()
 		self.right_list = QListView()
@@ -34,25 +37,41 @@ class MainWidget(QWidget):
 
 		self.add_button = QPushButton("Add")
 		self.rem_button = QPushButton("Remove")
+		self.test_button = QPushButton("Test")
+		
+		# Test image
+		self.graphics = QGraphicsView()
+		scene = QGraphicsScene()
+		map = QPixmap("img/test.jpg") # hardcoded path, images should be in db?
+		scene.addPixmap(map)
+		self.graphics.setScene(scene)
 
 		# Connect signals:
 		self.connect(self.search_line, SIGNAL("textChanged(QString)"),
  				self.left_list.model().filter)
-		self.connect(self.add_button, SIGNAL("clicked()"), self.add)
-		self.connect(self.rem_button, SIGNAL("clicked()"), self.remove)
-
+		self.connect(self.add_button, SIGNAL("clicked()"), self.add_clicked)
+		self.connect(self.rem_button, SIGNAL("clicked()"), self.remove_clicked)
+		self.connect(self.test_button, SIGNAL("clicked()"), self.show_image)
+		
 		# Create layout:
+		self.stack = QStackedWidget()
+		self.stack.insertWidget(0, self.right_list)
+		self.stack.insertWidget(1, self.graphics)
+
 		grid = QGridLayout()
 		grid.addWidget(self.search_line, 0, 0)
+		grid.addWidget(self.test_button, 0, 1)
 		grid.addWidget(self.left_list, 1, 0)
-		grid.addWidget(self.right_list, 1, 1)
+		grid.addWidget(self.stack, 1, 1)
 		grid.addWidget(self.add_button, 2, 0)
 		grid.addWidget(self.rem_button, 2, 1)
-		grid.addWidget(self.msg_field, 3, 1)
+		grid.addWidget(self.status, 3, 1)
+
 		self.setLayout(grid)
 
 		# Set widget parameters:
 		self.setWindowTitle('KiKrit')
+		#self.showFullScreen() # Should be used in final release
 		self.resize(600, 400)
 		self.move(200, 200)
 
@@ -66,27 +85,30 @@ class MainWidget(QWidget):
 
 
 	def getSelected(self, list_view):
-		"""Return a list of selected items
-
-		"""
+		"""Return a list of selected items"""
 		mdl = list_view.model()
 		return [mdl.items[i.row()] for i in list_view.selectedIndexes()]
 
 
-	def add(self):
-		"""Called when the user presses the add-button
-
-		"""
+	def add_clicked(self):
+		"""Called when the user presses the add-button"""
 		sel = self.getSelected(self.left_list)
 		#self.left_list.model().remove(sel)
 		self.right_list.model().add(sel)
 
 
-	def remove(self):
-		"""Called when the user presses the remove-button
-
-		"""
+	def remove_clicked(self):
+		"""Called when the user presses the remove-button"""
 		sel = self.getSelected(self.right_list)
 		removed = self.right_list.model().remove(sel)
 		#self.left_list.model().add(sel)
+	
+	def show_image(self):
+		cur = not(self.stack.currentIndex())
+		self.stack.setCurrentIndex(cur)
+		if cur:
+			self.status.setText("OK")
+		else:
+			self.status.setText("Waiting...")
+
 
