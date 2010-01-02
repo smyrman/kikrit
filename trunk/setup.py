8afeb9d7f98c3406389eb64f07c65b8e97ab4b4c
Sindre Røkenes Myren <smyrman@gmail.com>
2010-01-02 00:11:44 +0000

    fixed setup.py
    git-svn-id: https://omegav.no/svn/kikrit@50 77988d9d-5455-4c8b-b1c9-919ab32b2d28

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# This file supplies an easy wrapper for django_kikrit/manage.py, and contains
+# only a few essential commands.
 
 import os
 import sys
@@ -9,19 +11,22 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 os.chdir(PROJECT_ROOT)
 
 if sys.argv[1] == "help":
-	print "Commands:\n",
-		"help - show this text\n",
-		"install - install database according to settings and load initial",
-		"data\n",
+	print "Commands:\n"\
+		"help - show this text\n"\
+		"install - install database according to settings and load initial",\
+		"data\n"\
 		"backup - bacup all data from a to file."
+
 elif sys.argv[1] == "install":
 	# Create super-user, populate db with initial data:
-	cmd "django_kikrit/manage.py syncdb"
+	cmd = "django_kikrit/manage.py syncdb"
 	os.system(cmd.replace("/", os.path.sep))
+
 elif sys.argv[1] == "backup":
 	# Create super-user, populate db with initial data:
 	filename = "backup_%s.json" % datetime.datetime.now().isoformat()[:19]
 	cmd = "django_kikrit/manage.py dumpdata --format json > %s" % filename
 	os.system(cmd.replace("/", os.path.sep))
+
 else:
 	print "Command argument not found little man.. Try to add 'help'"
