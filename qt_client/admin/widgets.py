47e3be27f00d1a9a28a2effd4db0baf5ba3b8761
Sindre R. Myren <sindrero@shoebox.(none)>
2010-01-24 02:58:04 +0100

    Button layout changed, history deleted when home is clicked.

--- qt_client/admin/widgets.py
+++ qt_client/admin/widgets.py
@@ -24,40 +24,45 @@ class AdminWidget(QtGui.QWidget):
 	def __init__(self, rfid_thread, parent=None):
 		QtGui.QWidget.__init__(self, parent)
 		self.rfid_thread = rfid_thread
-	
+
 		# Define url's
 		self.home_url = QtCore.QUrl("http://localhost:%s/" % RUNSERVER_PORT)
-		self.logout_url = QtCore.QUrl("http://localhost:%s/logout" % RUNSERVER_PORT)
-		
+		self.logout_url = QtCore.QUrl("http://localhost:%s/logout" %
+				RUNSERVER_PORT)
+
 		# Views:
-		self.back_button = QtGui.QPushButton(getIcon("back", 32), "", self)
 		self.home_button = QtGui.QPushButton(getIcon("home", 32), "", self)
+		self.back_button = QtGui.QPushButton(getIcon("back", 32), "", self)
 		self.next_button = QtGui.QPushButton(getIcon("next", 32), "", self)
-		self.lock_button = QtGui.QPushButton(getIcon("lock", 32), "", self)
+		self.lock_button = QtGui.QPushButton(getIcon("lock", 32), "Logout", self)
 		self.web = QtWebKit.QWebView()
 
-		self.back_button.clicked.connect(self.back)
 		self.home_button.clicked.connect(self.home)
+		self.back_button.clicked.connect(self.back)
 		self.next_button.clicked.connect(self.next)
 		self.lock_button.clicked.connect(self.logout)
 		self.web.urlChanged.connect(self.urlChanged)
 		self.home()
-		
-		
+
+
 		# Layout:
 		grid = QtGui.QGridLayout(self)
-	
-		small = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed) # QSizePolicy(hor, vert)
-		self.back_button.setSizePolicy(small)
+
+		# QSizePolicy(hor, vert)
+		small = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed,
+				QtGui.QSizePolicy.Fixed)
+
 		self.home_button.setSizePolicy(small)
+		self.back_button.setSizePolicy(small)
 		self.next_button.setSizePolicy(small)
 		self.lock_button.setSizePolicy(small)
-		
-		grid.addWidget(self.back_button, 0, 3, 1, 1, QtCore.Qt.AlignRight) # add(row, col, row-size, col-size, align)
-		grid.addWidget(self.home_button, 0, 4, 1, 1, QtCore.Qt.AlignHCenter)
-		grid.addWidget(self.next_button, 0, 5, 1, 1, QtCore.Qt.AlignLeft)
-		grid.addWidget(self.lock_button, 0, 8, 1, 1, QtCore.Qt.AlignRight) 
-		grid.addWidget(self.web, 1, 0, 1, 9)
+
+		# addWidget(row, col, row-size, col-size, align)
+		grid.addWidget(self.home_button, 0, 0)
+		grid.addWidget(self.back_button, 0, 1)
+		grid.addWidget(self.next_button, 0, 2)
+		grid.addWidget(self.lock_button, 0, 4, 1, 1, QtCore.Qt.AlignRight)
+		grid.addWidget(self.web, 1, 0, 1, 5)
 		self.setLayout(grid)
 
 		# Connect signals:
@@ -70,21 +75,36 @@ class AdminWidget(QtGui.QWidget):
 		"""Opens the home page"""
 		self.web.load(self.home_url)
 
+
 	def back(self):
 		"""Back button clicked, go one page back"""
-		self.web.page().history().back()
-	
+		#self.web.page().history().back()
+		self.web.back()
+
+
 	def next(self):
 		"""Next button clicked, go to next page"""
-		self.web.page().history().forward()
-	
+		#self.web.page().history().forward()
+		self.web.forward()
+
+
 	def logout(self):
 		"""Logs out the user"""
 		self.web.load(self.logout_url)
-	
+
+
 	def urlChanged(self, new_url):
 		"""Updates fade/unfade back/next buttons"""
-		history = self.web.page().history()
+		#history = self.web.page().history()
+		history = self.web.history()
+
+		# Clear history on logout, and rigth after login:
+		print new_url
+		print self.home_url
+		if new_url == self.home_url:
+			history.clear()
+
+		# Update buttons:
 		if history.canGoBack():
 			self.back_button.setEnabled(True)
 		else:
