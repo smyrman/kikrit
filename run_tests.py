c069f88a11b3ea055865359baa9e6f508219deda
Sindre R. Myren <sindrero@shoebox.(none)>
2010-02-14 16:50:13 +0100

    Added transaction decorator to buy_merchandise function.

---
+++ run_tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010: Sindre Røkenes Myren, Andreas Hopland Sperre
+#
+# This file is part of KiKrit wich is distrebuted under GPLv3. See the file
+# COPYING.txt for more details.
+
+"""
+This file should always be run before commit!
+
+"""
+
+import os
+
+def main():
+	return os.system("django_kikrit/manage.py test merchandise accounts utils")
+
+if __name__ == "__main__":
+	main()
+
