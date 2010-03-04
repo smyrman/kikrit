280078c1c7a0bc27dcfd10bab116d7a1c409e753
Sindre Røkenes Myren <smyrman@gmail.com>
2010-03-04 18:33:47 +0100

    Fixed release script

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-KIKRIT_VERSION = "v0.1.2"
+KIKRIT_VERSION = "v0.1.3-pre-release"
 
 # Only increment when the database structure change:
 DATABASE_REVISION = 1 # No changes since KiKrit v0.1
