f5b263f8573be9b7a96f7cb4955e35fcc8cd6279
Sindre Røkenes Myren <smyrman@gmail.com>
2010-06-13 18:28:20 +0200

    Improved/fixed get_version functions

--- kikrit-server/kikrit_server/__init__.py
+++ kikrit-server/kikrit_server/__init__.py
@@ -5,17 +5,18 @@ version_info = (0, 3, 0, 'alpha', 0)
 
 def get_version(short=False):
 	"""If short==False, outputs:
-	'major.minor[.micro][ pre-alpha| alphaN| betaN]'
+	'major.minor[.micro][ pre-alpha| alpha N| beta N|rc N]'
 	If short==True, outputs:
-	'major.minor[.micro][aN|bN]'
+	'major.minor[.micro][aN|bN|rcN]'
+
 	"""
 	version = '%d.%d' % version_info[:2]
 	if version_info[2] != 0:
 		version += '.%d' % version_info[2]
-	if version_info[3] == 'final':
-		pass
-	elif short:
-		version += '%s%d' % (version_info[3][0], version_info[4])
+
+	if short:
+		mapping = { 'alpha':'a', 'beta':'b', 'rc':'rc', 'final':''}
+		version += '%s%d' % (mappiing[version_info[3]], version_info[4])
 	elif version_info[3] == 'alpha' and version_info[4] == 0:
 		version += ' pre-alpha'
 	else:
